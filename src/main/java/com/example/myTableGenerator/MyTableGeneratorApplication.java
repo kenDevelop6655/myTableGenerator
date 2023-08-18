678550d2f9e7db33d8abd8129973c08dc7d0b8a3
kenDevelop6655 <kenblack7715@gmail.com>
2023-08-18 10:28:48 +0900

    first commit

---
+++ src/main/java/com/example/myTableGenerator/MyTableGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.example.myTableGenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTableGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyTableGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/myTableGenerator/MyTableGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myTableGenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTableGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
